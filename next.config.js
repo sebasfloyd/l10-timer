0eee8691f694782c835d42c1fd94f4c919693e9b
sebasfloyd <sebachuffer@gmail.com>
2025-01-27 22:20:01 -0300

    Add security headers and environment configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://api.stripe.com https://checkout.stripe.com;"
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'browsing-topics=(), private-state-token-redemption=(), private-state-token-issuance=()'
+          }
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['api.stripe.com', 'checkout.stripe.com'],
+  }
 }
-
-module.exports = nextConfig
